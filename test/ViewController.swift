634ec26ed6bed4315b169dcfd58b3f8dda1cb814
yoshikik <yoshikik@kahokuzenki-no-MacBook-Air.local>
2014-11-11 00:59:25 +0900

    first commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet var label :UILabel
+    @IBOutlet var plusbtn :UIButton
+    @IBOutlet var minusbtn :UIButton
+    
+    
+    var number1: Int=0
+    var number2: Int=0
+    var result: Int=0
+    var phase: Int=0
+    
+    
+    @IBAction func one(){
+        if phase==0 {
+                number1 = 1
+                phase++
+                    }
+        else if phase==1 {
+            number1 = 1
+            phase++
+                    }
+        
+    }
+    
+    @IBAction func two(){
+        if phase==0 {
+            number1 = 2
+            phase++
+        }
+        else if phase==2 {
+            number1 = 2
+            phase++
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    
+    @IBAction func plus(){
+        if phase==2{
+        result=number1+number2
+            }
     }
-
-
+    @IBAction func minus(){
+        result=number1-number2
+        
+    }
+    @IBAction func kake(){
+        result=number1*number2
+        
+    }
+    @IBAction func waru(){
+        result=number1/number2
+        
+    }
+    
+    
+    @IBAction func write(){
+    label.text = "\(result)"
+        phase = 0
+    }
+    
+    
 }
 
